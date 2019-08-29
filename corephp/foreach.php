dfb94a41d0986061b7d6a61615025a3ca8bb4ca4
shreya66 <51993963+shreya66@users.noreply.github.com>
2019-08-29 13:08:45 +0530

    Delete foreach.php

--- corephp/foreach.php
+++
@@ -1,9 +0,0 @@
-<?php $employee = array("Employee1"=>60000,"Employee2"=>10000,"Employee3"=>30000,"Employee4"=>20000,"Employee5"=>50000,"Employee6"=>10000,"Employee7"=>31000,"Employee8"=>25000);
-
-
-foreach ($employee as $key=>$val) {
-if ($val >=30000) {
-echo $val ."<br>";
-}
-}
-?>
